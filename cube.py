21601a21912bb5e5c90eb92cb96f66122fc3260f
caleb-xerus <caleb.ho@me.com>
2016-05-27 00:43:49 -0400

    implement swap, shift, Cube.select

--- cube.py
+++ cube.py
@@ -4,6 +4,31 @@ Rubiks cube class
 
 import numpy as np
 
+def swap(d, key1, key2):
+    """Given a dictionary `d`, swap the values at keys `key1` and `key2`
+    """
+    d[key1], d[key2] = d[key2], d[key1]
+
+def shift(d, keys, dir_):
+    """Given a dictionary `d`, a sequence of keys `keys`, and a direction `dir`
+    move the values in `d` corresponding to `keys` one space over in the
+    direction `dir`
+
+    i.e.:
+    d = {'a':1, 'b':2, 'c':3}
+    right_shift(d) -> {'a':3, 'b':1, 'c':2}
+    """
+    if dir_ == 'r':
+        last = d[keys[-1]]
+        for i in range(0, len(keys) - 1):
+            d[keys[i + 1]] = d[keys[i]]
+        d[keys[0]] = last
+    else:
+        first = d[keys[0]]
+        for i in range(len(keys) - 1, 0, -1):
+            d[keys[i - 1]] = d[keys[i]]
+        d[keys[-1]] = first
+
 class Cube(object):
     def __init__(self):
         self._state = {'front': np.chararray((3,3)),
@@ -13,7 +38,7 @@ class Cube(object):
                       'up': np.chararray((3,3)),
                       'down': np.chararray((3,3))}
 
-        colors = ['b','g','r','o','y','w']
+        colors = ['b','g','r','o','y','w'] # each pair are opposite faces
         for face, color in zip(self._state.keys(), colors):
             self._state[face][:] = color
 
@@ -27,7 +52,7 @@ class Cube(object):
 
     def shuffle(self):
         pass
-        
+
     def is_solved(self):
         for face in self._state:
             vals = self._state[face].flatten()
@@ -38,7 +63,25 @@ class Cube(object):
         return True
 
     def select(self, face):
-        pass
+        """Selects a face for rotation
+        """
+        if face == 'front':
+            return
+        elif face == 'back':
+            swap(self._state, 'front', 'back')
+            swap(self._state, 'left', 'right')
+        elif face == 'right':
+            shift(self._state, ['left','front','right','back'], 'l')
+        elif face == 'left':
+            shift(self._state, ['left','front','right','back'], 'r')
+        elif face == 'up':
+            shift(self._state, ['down','front','up','back'], 'l')
+        elif face == 'back':
+            shift(self._state, ['down','front','up','back'], 'r')
 
-    def rotate(self, dir):
-        pass
+
+    def rotate(self, dir_):
+        """Rotates front face in `dir_` direction
+        """
+        if not self._selected:
+            raise RuntimeError("No face selected")
